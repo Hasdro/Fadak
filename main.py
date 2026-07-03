db03a3ce60844d77710c8a223abfd61d855f9d36
Hasan Chakeri <140052774+Hasdro@users.noreply.github.com>
2026-07-03 21:02:11 +0400

    Add Telegram API URL for sending messages

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main(context):
     
     req_body = context.req.body
     hasan_chat_id = "606109067";
+    tg_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     requests.post(tg_url, json={"chat_id": hasan_chat_id, "text": req_body})
     
     return context.res.json({"status": "ok"})
