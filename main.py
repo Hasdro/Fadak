6c664ad38e6077a3b8a38f21372547edb2e8584f
Hasan Chakeri <140052774+Hasdro@users.noreply.github.com>
2026-07-03 20:50:34 +0400

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def main(context):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    
+    req_body = context.req.body
+    hasan_chat_id = "606109067";
+    requests.post(tg_url, json={"chat_id": hasan_chat_id, "text": req_body})
+    
+    return context.res.json({"status": "ok"})
